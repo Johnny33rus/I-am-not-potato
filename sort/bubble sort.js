c6e66279d0c4457afdfd5c75e4950f89b89cafef
Johnny33san <johnny33san@gmail.com>
2021-12-25 22:02:35 +0300

    Add bubble sort for array in JS file

---
+++ sort/bubble sort.js
@@ -0,0 +1,17 @@
+function bubbleSort() {
+    let array = [4, 1, 6, 3, 5];
+        for (let i = 0; i < array.length; i++) {
+                for (let j = 0; j < array.length; j++) {
+                    if (array[j]>array[j+1]) {
+                        let p = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = p;
+                    }
+                    
+                }        
+        }
+        for (let i = 0; i < array.length; i++) {
+            console.log(array[i]);
+        }
+    }
+bubbleSort();
