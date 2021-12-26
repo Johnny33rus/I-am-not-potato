50535af8e095a0c849021a7a0adb98449023262e
Johnny33san <johnny33san@gmail.com>
2021-12-27 00:39:38 +0300

    add insert sort in C#

---
+++ sort/insertSort/insertSort/insertSort/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 4, 1, 6, 3, 5 };
+for (int i = 0; i < array.Length; i++)
+{
+    int v = array[i];
+    int j = i - 1;
+    while (j >= 0 && array[j] > v)
+        {
+        array[j+1] = array[j];
+        j--;
+        }
+        array[j+1] = v;
+}
+
+for (int i = 0; i < array.Length; i++)
+    {
+    Console.WriteLine(array[i]);
+    }
